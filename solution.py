1857ba1bcc1e947fd11c19b8adc5ee9992903d25
Vi Nguyen <vinguyen6@my.unt.edu>
2021-03-25 18:52:42 -0500

    add code to look up first pairs with duplicate output

--- solution.py
+++ solution.py
@@ -1,33 +1,36 @@
+#BIRTHDAY ATTACK
+#MESSAGE SIZE = 256 block
+
 from random import randint
 import hashlib
-
-input1 = 1  # TODO remove later
 
 # Declare array
 arr = [[], []]
 
-# Assign first row to input and second row to output
-inputRow = arr[0]
-outputRow = arr[1]
-
-while input1 != 2:  # TODO change condition
+for i in range(2):  # attack only need to runs at most 2 times
 
     # Save 2^20 random inputs and their BadHash40 outputs in arr
-    for i in range(1, 10):  # TODO actual range will be 1 to 2 ** 20
+    for i in range(1, 11):  # TODO actual range will be 1 to 2 ** 20 + 1
 
         # Get a random 256 bit number
-        x = randint(0, 2 ** 256)  # TODO shouldn't this be 2 ** 256 - 1?
-        inputRow.append(x)
+        x = randint(0, 21) # TODO actual range will be 0 to 2 ** 256
+        arr[0].append(x)
 
         # Perform BadHash40
         y = hashlib.sha256(
             str(x)
         )  # TODO I don't think this is taking only the first 40 bits!!
-        outputRow.append(y)
+        arr[1].append(y)
 
+    print("Before sort: ", str(arr)) # TESTING - remove when done!!
+    
     # Sort the outputs from smallest to largest
-    outputRow.sort()
+    arr[1].sort() # TODO update sort
+    
+    print("After sort: ", str(arr)) # TESTING - remove when done!!
 
-    input1 = 2  # TODO remove later
-
-print("final array: " + str(arr))  # TODO remove later
+    #look for the first 2 inputs that give out duplicate outputs
+    for i in range (len(arr[1])-1):
+        if arr[1][i] == arr[1][i+1]:
+            print("found pairs of input {" + hex(arr[0][i]) + ", " + hex(arr[0][i+1]) + "} that has the same output as " + arr[1][i].hexdigest())
+            break
