80d5f1f3396c24a67a4b23f9b8b03038af70eed1
Tanvi Mehta <tanvimehtaj@gmail.com>
2021-03-26 19:11:47 -0500

    changed array to numpy array

--- solution.py
+++ solution.py
@@ -3,10 +3,10 @@
 
 from random import randint
 import hashlib
-import numpy as np  # TODO maybe remove if not needed?
+import numpy as np
 
 # Declare array
-arr = [[], []]
+arr = np.empty((2, 0), int)
 
 for i in range(2):  # attack only need to runs at most 2 times
 
@@ -15,20 +15,24 @@ for i in range(2):  # attack only need to runs at most 2 times
 
         # Get a random 256 bit number
         x = randint(0, 20)  # TODO actual range will be 0 to 2 ** 256 - 1
-        arr[0].append(x)
+        # arr[0].append(x) # TODO remove
 
         # Perform BadHash40
         y = hashlib.sha256(str(x))
         #        print(y.hexdigest()) # TESTING - remove later
-        arr[1].append(y.hexdigest()[0:10])
 
-    #    print("Before sort: ", arr)  # TESTING - remove when done!!
+        col = [x, y.hexdigest()[0:10]]
+        arr = np.append(arr, np.array([col]).transpose(), axis=1)
+        # arr[1].append(y.hexdigest()[0:10]) #TODO remove
+
+    print("Before sort: ", arr)  # TESTING - remove when done!!
 
     # Sort the outputs from smallest to largest
 
+    sortedArr = arr[:, arr[1].argsort()]
     # arr[1].sort()  # TODO update sort to make sure first row indicies correspond with second row indices
 
-    #    print("After sort: ", arr)  # TESTING - remove when done!!
+    print("After sort: ", sortedArr)  # TESTING - remove when done!!
 
     # look for the first 2 inputs that give out duplicate outputs
     for i in range(len(arr[1]) - 1):
