bd82c262605438fd300bec24dab7afa6beaf9582
Vi Nguyen <vinguyen6@my.unt.edu>
2021-03-26 21:32:22 -0500

    Revert back to using regular array instead of numpy. Add comments. Cleanup

--- solution.py
+++ solution.py
@@ -1,45 +1,34 @@
-# BIRTHDAY ATTACK
+# BIRTHDAY ATTACK ON BADHASH40
 # MESSAGE SIZE = 256 block
+# BADHASH40 = first 40 bits of sha256 hash
+# Tanvi Mehta, Farhan Almufleh and Vi Nguyen
 
 from random import randint
+from sort import *
 import hashlib
-import numpy as np
 
 # Declare array
-arr = [[], []]  # np.empty((2, 0), int)
-
+arr = [[], []]
+#value to check when found duplicate output
 found = False
-for i in range(2):  # attack only need to runs at most 2 times
 
+for i in range(2):  # attack only need to runs at most 2 times
+    print("Generate input and hasing them...")
     # Save 2^20 random inputs and their BadHash40 outputs in arr
-    for i in range(1, 2 ** 20 + 1):  # TODO actual range will be 1 to 2 ** 20 + 1
+    for i in range(1, 2 ** 20 + 1):
 
         # Get a random 256 bit number
-        x = randint(0, 2 ** 256 - 1)  # TODO actual range will be 0 to 2 ** 256 - 1
+        x = randint(0, 2 ** 256 - 1)
         arr[0].append(x)
 
         # Perform BadHash40
         y = hashlib.sha256(str(x))
-        #        print(y.hexdigest()) # TESTING - remove later
-
-        # col = [x, y.hexdigest()[0:10]]
-        # print(col)
-        # arr = np.append(arr, np.array([col]).transpose(), axis=1)
         arr[1].append(y.hexdigest()[0:10])
 
-    print("Before sort: ", arr)  # TESTING - remove when done!!
+    print("Sorting through the array...")
+    heapSort(arr)
 
-    """# Sort the outputs from smallest to largest
-    for i in range(1, 2 ** 20 + 1):
-        for j in range(0, (2 ** 20 - i)):
-            if arr[1][j] > arr[1][j + 1]:
-                arr[1][j], arr[1][j + 1] = arr[1][j + 1], arr[1][j]
-                arr[0][j], arr[0][j + 1] = arr[0][j + 1], arr[0][j]
-    # sortedArr = arr[:, arr[1].argsort()]
-    """  # arr[1].sort()  # TODO update sort to make sure first row indicies correspond with second row indices
-
-    print("After sort: ", arr)  # TESTING - remove when done!!
-
+    print("Looking for duplicate output")
     # look for the first 2 inputs that give out duplicate outputs
     for i in range(len(arr[1]) - 1):
         if arr[1][i] == arr[1][i + 1]:
@@ -48,10 +37,11 @@ for i in range(2):  # attack only need to runs at most 2 times
                 + hex(arr[0][i])
                 + ", "
                 + hex(arr[0][i + 1])
-                + "} that has the same output as "
+                + "} that has the same output "
                 + (arr[1][i])
             )
             found = True
             break
     if found == True:
         break
+    print("\n")
--- sort.py
+++ sort.py
@@ -1,14 +1,35 @@
-def mergeSort(arr):
-    if len(arr[1]) > 1:
-        mid = len(arr[1]) // 2
-
-        L = arr[1][:mid]
-        R = arr[1][mid:]
-        
-        mergeSort(L)
-        mergeSort(R)
-
-        i=j=k=0
-
-        while i<len(L) and j<len(R):
-            if L[i] < R[j]
+#heapify function
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1     # left = 2*i + 1
+    r = 2 * i + 2     # right = 2*i + 2
+  
+    # See if left child of root exists and if greater than root
+    if l < n and arr[1][i] < arr[1][l]:
+        largest = l
+  
+    # See if right child of root exists and if greater than root
+    if r < n and arr[1][largest] < arr[1][r]:
+        largest = r
+  
+    # Change root, if needed
+    if largest != i:
+        arr[1][i],arr[1][largest] = arr[1][largest],arr[1][i]  # swap output values
+        arr[0][i],arr[0][largest] = arr[0][largest],arr[0][i]  # swap input values
+        # Heapify the root.
+        heapify(arr, n, largest)
+  
+#heapsort function
+def heapSort(arr):
+    n = len(arr[1])
+  
+    # Build a maxheap.
+    # Since last parent will be at ((n//2)-1) we can start at that location.
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+  
+    # One by one extract elements
+    for i in range(n-1, 0, -1):
+        arr[1][i], arr[1][0] = arr[1][0], arr[1][i]   # swap output values
+        arr[0][i], arr[0][0] = arr[0][0], arr[0][i]   # swap input values
+        heapify(arr, i, 0)
