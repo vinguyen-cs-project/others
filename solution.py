4a448334a3e89c822c413f3513ce590c3299bf8f
Vi Nguyen <vinguyen6@my.unt.edu>
2021-03-26 20:10:26 -0500

    Finish code

--- solution.py
+++ solution.py
@@ -8,13 +8,14 @@ import numpy as np
 # Declare array
 arr = np.empty((2, 0), int)
 
+found = False
 for i in range(2):  # attack only need to runs at most 2 times
 
     # Save 2^20 random inputs and their BadHash40 outputs in arr
-    for i in range(1, 10):  # TODO actual range will be 1 to 2 ** 20 + 1
+    for i in range(1, 2**20+1):  # TODO actual range will be 1 to 2 ** 20 + 1
 
         # Get a random 256 bit number
-        x = randint(0, 20)  # TODO actual range will be 0 to 2 ** 256 - 1
+        x = randint(0, 2**256-1)  # TODO actual range will be 0 to 2 ** 256 - 1
         # arr[0].append(x) # TODO remove
 
         # Perform BadHash40
@@ -22,6 +23,7 @@ for i in range(2):  # attack only need to runs at most 2 times
         #        print(y.hexdigest()) # TESTING - remove later
 
         col = [x, y.hexdigest()[0:10]]
+        print(col)
         arr = np.append(arr, np.array([col]).transpose(), axis=1)
         # arr[1].append(y.hexdigest()[0:10]) #TODO remove
 
@@ -35,16 +37,18 @@ for i in range(2):  # attack only need to runs at most 2 times
     print("After sort: ", sortedArr)  # TESTING - remove when done!!
 
     # look for the first 2 inputs that give out duplicate outputs
-    for i in range(len(arr[1]) - 1):
-        if arr[1][i] == arr[1][i + 1]:
+    for i in range(len(sortedArr[1]) - 1):
+        if sortedArr[1][i] == sortedArr[1][i + 1]:
             print(
-                "found pairs of input {"
-                + hex(arr[0][i])
-                + ", "
-                + hex(arr[0][i + 1])
-                + "} that has the same output as "
-                + arr[1][
-                    i
-                ]  # .hexdigest() # Vi, we removed hexdigest here, as we've done it above. feel free to change it back if needed
+                "found pairs of input {" +
+                hex(sortedArr[0][i])+ ", "+
+                hex(sortedArr[0][i + 1])
+                +
+                "} that has the same output as "
+                + (sortedArr[1][i])
             )
+            found = True
             break
+    if found == True:
+        break
+    
